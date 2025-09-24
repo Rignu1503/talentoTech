f6217f8434913c79850a194f241a2bd917386585
Rignu1503 <rigoberto.miranda1503@gmail.com>
2025-09-24 12:54:55 -0500

    feat(exercises 9):add exercises solved

--- practical-exercises/src/main/java/dev/Rignu1503/exercises/example/exercise9.java
+++ practical-exercises/src/main/java/dev/Rignu1503/exercises/example/exercise9.java
@@ -1,25 +1,55 @@
 package dev.Rignu1503.exercises.example;
-
 import java.util.ArrayList;
 
 public class exercise9 {
 
-     public ArrayList<Integer> ascendingList(int num1, int num2, int num3){
-
+    public ArrayList<Integer> ascendingList(int num1, int num2, int num3) {
         ArrayList<Integer> ascending = new ArrayList<>();
 
-        if(num1 >= num2 && num1 >= num3){
-            ascending.add(num1);
+        int menor, medio, mayor;
+
+        if (num1 <= num2 && num1 <= num3) {
+            menor = num1;
+            if (num2 <= num3) {
+                medio = num2;
+                mayor = num3;
+            } else {
+                medio = num3;
+                mayor = num2;
+            }
+        } else if (num2 <= num1 && num2 <= num3) {
+            menor = num2;
+            if (num1 <= num3) {
+                medio = num1;
+                mayor = num3;
+            } else {
+                medio = num3;
+                mayor = num1;
+            }
+        } else {
+            menor = num3;
+            if (num1 <= num2) {
+                medio = num1;
+                mayor = num2;
+            } else {
+                medio = num2;
+                mayor = num1;
+            }
         }
-        
+
+        // Agregar en orden ascendente
+        ascending.add(menor);
+        ascending.add(medio);
+        ascending.add(mayor);
+
         return ascending;
     }
 
     public static void main(String[] args) {
-
         exercise9 iv = new exercise9();
-        System.out.println(iv.ascendingList(-3, 2, 9));
 
+        System.out.println(iv.ascendingList(9, 50, 4)); //Salida [4, 9, 50]
+        System.out.println(iv.ascendingList(-3, 2, 9));   //Salida [-3, 2, 9]
+        System.out.println(iv.ascendingList(7, 7, 1)); //Salida [1, 7, 7]
     }
-    
 }
